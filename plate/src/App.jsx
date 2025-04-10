c791a6a03c6b88820f62f60db721d945e6a14184
Nat Tuck <nat@ferrus.net>
2025-04-10 10:34:00 -0400

    changes

--- plate/src/App.jsx
+++ plate/src/App.jsx
@@ -1,22 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import appLogo from '/favicon.svg'
-import PWABadge from './PWABadge.jsx'
-import './App.css'
+import { useState } from 'react';
+import appLogo from '/favicon.svg';
+import PWABadge from './PWABadge.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={appLogo} className="logo" alt="plate logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+    <div className="card shadow-sm">
+      <div></div>
       <h1>plate</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -30,8 +21,8 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <PWABadge />
-    </>
+    </div>
   )
 }
 
-export default App
+export default App;
