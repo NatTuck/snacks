2c73feffe7dda733c80ac422dd8e4ea24326ed65
Nat Tuck <nat@ferrus.net>
2025-04-10 14:00:31 -0400

    pizza

--- plate/src/App.jsx
+++ plate/src/App.jsx
@@ -1,26 +1,30 @@
 import { useState } from 'react';
-import appLogo from '/favicon.svg';
+import appLogo from './assets/pizza.svg';
 import PWABadge from './PWABadge.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="card shadow-sm">
-      <div></div>
-      <h1>plate</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <div className="flex justify-center w-screen">
+      <div className="card shadow-sm lg:w-3/4">
+        <div className="flex">
+          <img className="w-20 h-20" src={appLogo} />
+        </div>
+        <h1>plate</h1>
+        <div className="card">
+          <button className="btn btn-neutral w-50" onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
+        <PWABadge />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <PWABadge />
     </div>
   )
 }
