f855c9c29110cf4532ef66a2283d73a4c7035996
Nat Tuck <nat@ferrus.net>
2025-04-21 14:57:51 -0400

    date picker

--- plate/src/snacks-page.jsx
+++ plate/src/snacks-page.jsx
@@ -1,10 +1,35 @@
 
+import { useState } from 'react';
+import { format, startOfDay } from 'date-fns';
+import { DayPicker } from 'react-day-picker';
 
 function SnacksPage() {
+  const [date, setDate] = useState(startOfDay(Date()));
+
   return (
     <div>
-      <p>Select Date</p>
-      <p></p>
+      <div className="flex">
+        <span className="m-2">
+          {format(date, "EEEE, yyyy-MM-dd")}
+        </span>
+        <button className="btn" popoverTarget="day-picker-po">
+          Calendar
+        </button>
+        <div
+          popover="auto"
+          id="day-picker-po"
+          className="dropdown"
+          style={{ positionAnchor: "--rdp" }}>
+          <DayPicker
+            className="react-day-picker"
+            mode="single"
+            selected={date}
+            onSelect={setDate} />
+        </div>
+      </div>
+      <p>Summary</p>
+      <p>Add Snack</p>
+      <p>Snacks List</p>
     </div>
   );
 }
